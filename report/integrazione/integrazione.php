fbb6b91592f6278296d35f3060646d373c004613
giuseppefiscarelli <57521642+giuseppefiscarelli@users.noreply.github.com>
2021-09-07 21:16:27 +0200

    Update integrazione.php

--- report/integrazione/integrazione.php
+++ report/integrazione/integrazione.php
@@ -13,10 +13,14 @@
 require_once  '../../vendor/autoload.php';
 require_once  '../../model/istanze.php';
 //require_once  '../../functions.php';
+/*
 $rep = getReportId($_GET['id']);
 $user = getistanza($rep['id_RAM']);
 $dettagli = getDettReport($_GET['id']);
+*/
 $tipo = $_GET['tipo'];
+
+
 //var_dump($rep);die; 
 
 use Spipu\Html2Pdf\Html2Pdf;
