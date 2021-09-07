9f44ae72077d40af254bee5afc4301a69bb0ed29
giuseppefiscarelli <57521642+giuseppefiscarelli@users.noreply.github.com>
2021-09-07 22:30:51 +0200

    Update integrazione.php

--- report/integrazione/res/integrazione.php
+++ report/integrazione/res/integrazione.php
@@ -88,7 +88,7 @@
     </page_footer>
     <page_header> 
         <table width="75%" style="margin:30px;">
-            <tr><td><img style="width:180px;display:inline;"  src="../../images/RAM_nuovo_logo.png"></td></tr>
+            <tr><td><img style="width:180px;display:inline;"  src="../../images/RAM nuovo logo.png"></td></tr>
             <tr><td style="font-family: 'Times New Roman', Times, serif;text-align:center;">Direttore Operativo</td>
             </tr>
 
