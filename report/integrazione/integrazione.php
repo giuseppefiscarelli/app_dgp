1d2fa90599089e8edcd94e47c973d5565982ba0c
giuseppefiscarelli <57521642+giuseppefiscarelli@users.noreply.github.com>
2021-09-07 21:56:48 +0200

    pdf

--- report/integrazione/integrazione.php
+++ report/integrazione/integrazione.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /**
  * Html2Pdf Library - example
  *
@@ -10,22 +10,27 @@
  * @author    Laurent MINGUET <webmaster@html2pdf.fr>
  * @copyright 2017 Laurent MINGUET
  */
-require_once  '../../vendor/autoload.php'; 
+require_once  '../../vendor/autoload.php';
 require_once  '../../model/istanze.php';
-var_dump($_GET); die;
-/*
 require_once  '../../functions.php';
-
-
 $rep = getReportId($_GET['id']);
 $user = getistanza($rep['id_RAM']);
 $dettagli = getDettReport($_GET['id']);
-
 $tipo = $_GET['tipo'];
+/*
+//$contrId = intval($data['id_contratto']);
+$contrId = intval($_GET['id']);
+//var_dump($contrId);die;
+$contr = getContratto($contrId);
 
+$cliId = $contr['id_cliente'];
+$motoId = $contr['id_veicolo'];
+$cli= getClientecf($cliId);
+$m=getMotosel($motoId);
+$acc=getAccessori($contrId);
+//$quest = getValutazione($trId);
+//var_dump($tr);die;
 */
-//var_dump($rep);die; 
-
 use Spipu\Html2Pdf\Html2Pdf;
 use Spipu\Html2Pdf\Exception\Html2PdfException;
 use Spipu\Html2Pdf\Exception\ExceptionFormatter;
@@ -34,21 +39,17 @@ try {
     $html2pdf = new Html2Pdf('P', 'A4', 'en', true, 'UTF-8', array(5, 5, 5, 2),true);
     $html2pdf->pdf->SetDisplayMode('fullpage');
     $html2pdf->pdf->SetProtection(array('print','copy'));
-    
-    
     $html2pdf->setDefaultFont('times', 'serif');
     ob_start();
     include dirname(__FILE__).'/res/integrazione.php';
     
      
-    $pathReport= "/inv2020/report/";
+    
     $content = ob_get_clean();
     $path = $pathReport;
     $html2pdf->writeHTML($content);
     $filename = $rep['id']."_".$rep['id_RAM']."_".time();
-    $html2pdf->output();
-    
-    
+    //$html2pdf->createIndex('Sommaire', 30, 12, false, true, 2, null, '10mm');
     if($tipo =="P"){
         $html2pdf->output($filename.".pdf",'I');
     }
