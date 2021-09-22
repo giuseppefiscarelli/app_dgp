9f93c7fcdb1976b96da28212e360045e6d3e114d
giuseppefiscarelli <57521642+giuseppefiscarelli@users.noreply.github.com>
2021-09-22 15:37:49 +0200

    Update testmail2.php

--- testmail2.php
+++ testmail2.php
@@ -1,6 +1,5 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -12,25 +11,21 @@ try {
     //Server settings
     //$mail->SMTPDebug = 2;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtps.pec.aruba.it';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'n.salvatore@pec.it';                 // SMTP username
-    $mail->Password = 'NicAruba@1959';                           // SMTP password
+    $mail->Username = 'ram.investimenti2020@legalmail.it';                 // SMTP username
+    $mail->Password = 'R4ns@2020_VII';                           // SMTP password
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('n.salvatore@pec.it');
+    $mail->setFrom('ram.investimenti2020@legalmail.it');
     $mail->addAddress('n.salvatore@gmail.com');     // Add a recipient
     $mail->addAddress('fiscarelli.giu@gmail.com');               // Name is optional
-    //$mail->addReplyTo('info@example.com', 'Information');
-   // $mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
+   
 
     //Attachments
     $mail->addAttachment('/investimentivii/report/193__1627461928.pdf');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
+    
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Here is the subject';
