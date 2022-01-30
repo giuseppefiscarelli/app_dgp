999aa4390c31b3817d5fa9af2e177889ff963eff
giuseppefiscarelli <57521642+giuseppefiscarelli@users.noreply.github.com>
2022-01-30 20:11:33 +0100

    new

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'mysql_host' => 'localhost',
+    'mysql_user' => 'root',
+    'mysql_password' => '',
+    'mysql_db' => 'dgp',
+    'recordsPerPage' => 10,
+    'recordsPerPageOptions' => [
+        5,10,15,20,30,50,100
+    ],
+    'roletype' => [
+        'admin','user','suadmin'
+
+    ],
+    'numLinkNavigator' => 4,
+    $pathAlle = "xxxxx/inv2020/allegati/",  // rimuovere xxxxx
+    $pathEml = 'xxxxxd:\\APPO_TEST\\'
+]
+
+;
